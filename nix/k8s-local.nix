0dd79115bae70b25db25709168329e50d3bcb717
Damian Baar <damian.baar@gmail.com>
2019-05-20 16:28:41 +0200

    bootstrapping local env with kind

---
+++ nix/k8s-local.nix
@@ -0,0 +1,23 @@
+{ pkgs, env-config }:
+rec {
+  create-local-cluster = pkgs.writeScript "create-local-cluster" ''
+    ${pkgs.kind}/bin/kind delete cluster || true
+    ${pkgs.kind}/bin/kind create cluster --name ${env-config.projectName}
+  '';
+
+  create-local-cluster-if-not-exists = pkgs.writeScriptBin "create-local-cluster-if-not-exists" ''
+    echo "Checking existence of cluster ..."
+    ${pkgs.kind}/bin/kind get clusters | grep ${env-config.projectName} || ${create-local-cluster}
+  '';
+
+  deploy-to-kind = {config, image}: 
+    pkgs.writeScriptBin "deploy-to-kind" ''
+      echo "Loading the ${pkgs.docker}/bin/docker image inside the kind docker container ..."
+
+      kind load image-archive ${image}
+      echo "Applying the configuration ..."
+
+      cat ${config} | ${pkgs.jq}/bin/jq "."
+      cat ${config} | ${pkgs.kubectl}/bin/kubectl apply -f -
+    '';
+}
--- shell.nix
+++ shell.nix
@@ -1,3 +1,4 @@
+{ }:
 let
   pkgs = (import ./nix {}).pkgs;
 in
@@ -10,8 +11,16 @@ mkShell {
     nodejs
     pkgs.yarn2nix.yarn
     pkgs.functions.scripts.build-and-push
+    pkgs.k8s-local.create-local-cluster-if-not-exists
+    pkgs.kind
   ];
 
   shellHook= ''
+    echo "Hey sailor!"
+
+    create-local-cluster-if-not-exists
+
+    export PROJECT_NAME="${pkgs.env-config.projectName}"
+    export KUBECONFIG=$(${pkgs.kind}/bin/kind get kubeconfig-path --name=$PROJECT_NAME)
   '';
 }
