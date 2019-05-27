6fbf9228725ff915bb77b7cb7ed80bf844eeae15
Damian Baar <damian.baar@wipro.com>
2019-05-27 12:15:18 +0200

    added expose-ingress, added multicluster configuration, added knctl

--- functions/express-app/src/index.js
+++ functions/express-app/src/index.js
@@ -8,6 +8,12 @@ app.get("/", (req, res) => {
   res.send(`Hello ${target}!`)
 })
 
+// https://kubernetes.io/docs/tasks/configure-pod-container/configure-liveness-readiness-probes/#define-a-liveness-http-request
+app.get("/healthz", (req, res) => {
+  console.log("Calling health check endpoint")
+  res.status(200).send("ok")
+})
+
 const port = process.env.PORT || 8080
 app.listen(port, () => {
   console.log("Hello world listening on port", port)
--- nix/k8s-local.nix
+++ nix/k8s-local.nix
@@ -1,13 +1,32 @@
-{ pkgs, env-config }:
+{ pkgs, lib, env-config, kubenix }:
 rec {
   delete-local-cluster = pkgs.writeScriptBin "delete-local-cluster" ''
     echo "Deleting cluster"
     ${pkgs.kind}/bin/kind delete cluster --name ${env-config.projectName} || true
   '';
 
+  cluster-config = {
+    kind = "Cluster";
+    apiVersion = "kind.sigs.k8s.io/v1alpha3";
+    nodes = [
+      { 
+        role = "control-plane"; 
+        extraMounts = [{
+          containerPath = "/kind-source";
+          hostPath = toString ./.;
+          readOnly = true;
+        }];
+      }
+      { role = "worker"; }
+      { role = "worker"; }
+    ];
+  };
+
+  cluster-config-yaml = kubenix.lib.toYAML cluster-config;
+
   create-local-cluster = pkgs.writeScript "create-local-cluster" ''
     echo "Creating cluster"
-    ${pkgs.kind}/bin/kind create cluster --name ${env-config.projectName}
+    ${pkgs.kind}/bin/kind create cluster --name ${env-config.projectName} --config ${cluster-config-yaml}
   '';
 
   create-local-cluster-if-not-exists = pkgs.writeScriptBin "create-local-cluster-if-not-exists" ''
@@ -15,12 +34,24 @@ rec {
     ${pkgs.kind}/bin/kind get clusters | grep ${env-config.projectName} || ${create-local-cluster}
   '';
 
+  getPortScript = "$(kubectl get svc $INGRESSGATEWAY --namespace istio-system   --output 'jsonpath={.spec.ports[?(@.port==80)].nodePort}')";
+
+  expose-istio-ingress = pkgs.writeScriptBin "expose-ingress" ''
+    NODE_PORT=${env-config.ports.istio-ingress}
+    echo "exposing port $NODE_PORT"
+    ${pkgs.kubectl}/bin/kubectl --namespace istio-system port-forward service/istio-ingressgateway $NODE_PORT:80 > /dev/null &
+  '';
+
+# # get local IP:  
+# # export IP_ADDRESS=$(kubectl get node  --output 'jsonpath={.items[0].status.addresses[0].address}'):
+# # $(kubectl get svc $INGRESSGATEWAY --namespace istio-system   --output 'jsonpath={.spec.ports[?(@.port==80)].nodePort}')
+
+# {writeScriptBin}: {
+# # kubectl -n garden port-forward service/etcd 32379:2379 > /dev/null &  
+# }
+
   export-kubeconfig = pkgs.writeScriptBin "export-kubeconfig" ''
     export KUBECONFIG=$(${pkgs.kind}/bin/kind get kubeconfig-path --name=${env-config.projectName})
-
-    export CLUSTER_NODE_PORT=$(kubectl get svc $INGRESSGATEWAY --namespace istio-system  --output 'jsonpath={.spec.ports[?(@.port==80)].nodePort}')
-    export CLUSTER_HOST=$(kubectl get node --output 'jsonpath={.items[0].status.addresses[0].address}')
-    export CLUSTER_IP_ADDRESS=$CLUSTER_HOST:$CLUSTER_NODE_PORT
   '';
 
   deploy-to-kind = {config, image}: 
