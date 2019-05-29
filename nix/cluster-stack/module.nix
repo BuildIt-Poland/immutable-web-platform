7f768628421588d34b781c26af1b58a4ead9eb89
Damian Baar <damian.baar@gmail.com>
2019-05-29 08:59:09 +0200

    reenabling ns for brigade - seems that gateway should handle this well

--- nix/cluster-stack/module.nix
+++ nix/cluster-stack/module.nix
@@ -13,14 +13,18 @@ in
   # most likely bitbucket gateway does not handle namespace -> envvar BRIGADE_NAMESPACE
   # perhaps need to pass it somehow during creation -> invetigate
   kubernetes.helm.instances.brigade = {
-    # namespace = "${namespace}";
+    namespace = "${namespace}";
     chart = charts.brigade;
     # values = {
     # };
   };
 
   # INFO json cannot be applied here as it is handled via helm module
+
   # https://github.com/lukepatrick/brigade-bitbucket-gateway/blob/master/charts/brigade-bitbucket-gateway/values.yaml
+
+  # handling env var
+  # https://github.com/lukepatrick/brigade-bitbucket-gateway/blob/master/brigade-bitbucket-gateway/cmd/brigade-bitbucket-gateway/server.go#L71
 
   # TODO base64 X-Hook-UUID
   # TODO EDITOR=nvim kubectl edit roles brigade-bitbucket-gateway-brigade-bitbucket-gateway
@@ -47,7 +51,7 @@ in
   #   verbs:
   #   - '*'
   kubernetes.helm.instances.brigade-bitbucket-gateway = {
-    # namespace = "${namespace}";
+    namespace = "${namespace}";
     name = "brigade-bitbucket-gateway";
     chart = charts.brigade-bitbucket;
     values = {
