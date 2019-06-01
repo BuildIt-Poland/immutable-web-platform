cc2accd167df096f4202e1b0e68e0402a80d2ff6
Damian Baar <damian.baar@gmail.com>
2019-06-01 16:37:08 +0200

    adding custom worker image for brigade

--- docs/cache.md
+++ docs/cache.md
@@ -3,7 +3,11 @@ https://docs.brigade.sh/topics/javascript/
 
 https://github.com/brandon-bethke-neudesic/brigade/blob/cef43b38fad83975da817245f6bc6167e90dea42/brigade-worker/src/k8s.ts#L43
 
+ •  kubernetes.cacheStorageClass: This is used for the Job cache.
+ •  kubernetes.buildStorageClass: This is used for the shared per-build storage.
+
+
 Idea is that:
-* each build is doing rsync to cache/build storage
+* each build is doing rsync to cache/build storage -- there is no way to do this without endpoint for upload since there is no shared space between workers - need to write custom worker with embeded pvc - easy ...
 * storage is mounted to remote-worker
 * remote-work act as remote-binary-store
---
+++ nix/cluster-stack/brigade-worker.nix
@@ -0,0 +1,38 @@
+{ linux-pkgs, env-config }:
+let
+  image = "brigadecore/brigade-worker";
+  port = 5000;
+  pkgs = linux-pkgs;
+  # skopeo inspect docker://docker.io/brigadecore/brigade-worker
+  # THIS IS MAGIC!
+  worker = pkgs.dockerTools.pullImage {
+    imageName = image;
+    imageDigest = "sha256:94c85a0f50bf687c1b5ac88ae6a78b28720664efb13a096546cd8cb759820527";
+    sha256 = "1r4ngh98ir7q2a0rlhjjgpp4hpmmi9y94lgq7n27vrwnzkjlqlvc";
+    os = "linux";
+    arch = "amd64";
+  };
+in
+# INFO: this image is required to embed custom scripts
+pkgs.dockerTools.buildImage ({
+  name = "dev.local/brigade-worker";
+  tag = "latest";
+
+  fromImage = worker;
+  # fromImageTag = "latest";
+
+  extraCommands = ''
+    ${pkgs.yarn}/bin/yarn add xml-simple
+  '';
+
+  config = {
+    Cmd = ["yarn run test"];
+    WorkingDir = "/home/src";
+  };
+
+  contents = [
+    pkgs.bash
+    pkgs.coreutils
+    pkgs.yarn
+  ];
+})
