b4e1870530db9793527ad6ee9efffa388643470d
Damian Baar <damian.baar@wipro.com>
2019-06-10 17:06:52 +0200

    command line for locker

--- packages/remote-state/src/index.ts
+++ packages/remote-state/src/index.ts
@@ -1,14 +1,26 @@
 import { aws } from './provisioner'
+import * as yargs from 'yargs'
 
-// console.log(aws.setLock(true))
-console.log('@@@', process.cwd())
+yargs
+  .command('lock', 'Lock state', {}, () => aws.setLock(true))
+  .command('unlock', 'Unlock state', {}, () => aws.setLock(false))
 
-aws.setLock(true)
-  .then(() => aws.getLockState())
-  .then(console.log)
-  .then(() => aws.setLock(false))
-  .then(() => aws.getLockState())
-  .then(console.log)
-  .then(() => aws.uploadState('./test.nixops'))
-  .then(() => aws.getStateFromBucket('test.nixops'))
-  .then(console.log)
+  .command('status', 'Get status of locker', { dump: {} },
+    ({ dump }) =>
+      (dump
+        ? aws.getLockState()
+        : aws.getLockState().then(d => d.locked))
+        .then(console.log)
+  )
+
+  .command('upload-state', 'Upload state', { file: {} },
+    ({ file }) =>
+      aws.uploadState(file as string))
+
+  .command('download-state', 'Download state', { file: {} },
+    ({ file }) =>
+      aws.getStateFromBucket(file as string))
+
+  .demandCommand()
+  .help()
+  .argv
