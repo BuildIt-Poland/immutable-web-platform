33b49b23f2d4e812aa6ac79a376e5cb5d68ced18
Damian Baar <damian.baar@wipro.com>
2019-06-11 11:50:29 +0200

    adding pretty diff

--- packages/remote-state/src/index.ts
+++ packages/remote-state/src/index.ts
@@ -3,11 +3,13 @@ import * as yargs from 'yargs'
 import { isStateDiffers, diffState, fileToJSON, reconcileState } from './reconciler'
 import * as shell from 'shelljs'
 import * as inquirer from 'inquirer'
-import { basename } from 'path';
+import { basename } from 'path'
+import { diffString as prettyDiff, diff } from 'json-diff'
 
+const remoteStateFileName = "localstate.nixops.json"
 const defaultRemote = {
   remote: {
-    default: "localstate.nixops.json"
+    default: remoteStateFileName
   }
 }
 
@@ -26,50 +28,60 @@ yargs
   // locker upload-state ./test/remote-state-a.json --before-upload "cat" -> it is going to read stdout
   .command('upload-state <local>', 'Upload state', {
     local: {},
+    remoteFileName: { default: basename(remoteStateFileName) },
     force: { default: false },
-    beforeUpload: {},
+    beforeUpload: {
+      defaultDescription: "Extra command before uploading - data are passed thru stdin, to be used i.e for encryption before send",
+    },
     ...defaultRemote,
   },
-    ({ local, remote, force, beforeUpload }) =>
-      // TODO check if file exists
+    ({ local, remote, force, beforeUpload, remoteFileName }) =>
       force
-        ? aws.uploadState(local as string)
-        : aws.getStateFromBucket(remote as string)
+        ? aws.uploadState(local as string, remoteFileName)
+        : aws
+          .getStateFromBucket(remote as string)
           .then(remoteState => {
             const localState = fileToJSON(local as string)
-            const diff = diffState(remoteState, localState)
+            const diff = diffState(remoteState as string, localState)
 
-            if (isStateDiffers(diff)) {
-              console.log('you have changes', JSON.stringify(diff.changes, null, 2))
-              return inquirer.prompt<{ proceed: boolean }>([
-                {
-                  type: 'confirm',
-                  name: 'proceed',
-                  message: 'remote state differs, should merge state as?',
-                  default: false
-                }
-              ]).then(answers => {
-                if (!answers.proceed) {
-                  console.log('Aborting action')
-                  return
-                }
-
-                const a = beforeUpload && shell
-                  .echo(JSON.stringify(diff))
-                  .exec(beforeUpload as string)
-
-                if (a && a.stdout) {
-                  return aws.uploadStateFromStdout(a.stdout, basename(remote as string))
-                } else {
-                  const reconciled = reconcileState(diff)
-                  return aws.uploadStateFromStdout(
-                    JSON.stringify(reconciled, null, 2),
-                    basename(remote as string)
-                  )
-                }
-              })
+            if (!isStateDiffers(diff)) {
+              console.log('State seems to be the same - skipping upload')
+              return
             }
+
+            console.log('Changes to be aplied\n', prettyDiff(JSON.parse(remoteState), JSON.parse(localState)))
+            return inquirer.prompt<{ proceed: boolean }>([
+              {
+                type: 'confirm',
+                name: 'proceed',
+                message: 'remote state differs, should merge state as?',
+                default: false
+              }
+            ]).then(answers => {
+              if (!answers.proceed) {
+                console.log('Aborting action')
+                return
+              }
+
+              const a = beforeUpload && shell
+                .echo(JSON.stringify(diff))
+                .exec(beforeUpload as string)
+
+              if (a && a.stdout) {
+                return aws.uploadStateFromStdout(a.stdout, remoteFileName)
+              } else {
+                const reconciled = reconcileState(diff)
+                return aws.uploadStateFromStdout(
+                  JSON.stringify(reconciled, null, 2),
+                  basename(remote as string)
+                )
+              }
+            })
           })
+          .catch(e =>
+            aws.uploadState(local as string, remoteFileName) // INFO: key does not exists
+              .then(() => console.log('State update complete.'))
+          )
   )
 
   .command('download-state', 'Download state', { file: {} },
