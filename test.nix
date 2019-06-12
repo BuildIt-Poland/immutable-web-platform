0fe286391340df86b33b07863b76180afed01c76
Damian Baar <damian.baar@gmail.com>
2019-06-12 15:56:37 +0200

    trying to setup nixos tests

--- test.nix
+++ test.nix
@@ -1,18 +1,39 @@
 # INFO not going to use make-test since it requrie kvm - will be run in kind -> docker
-{ nixpkgs }:
+# { nixpkgs }:
+# let
+#   pkgs = (import ./nix {}).pkgs;
+#   testFunction = { pkgs, ... }: {
+#     name = "test";
+
+#     nodes = {
+#     };
+
+#     testScript = ''
+#     '';
+#   };
+#   nixosTesting = import "${nixpkgs}/lib/testing.nix" {
+#     inherit pkgs;
+#     system = "x86_64-linux";
+#   };
+# in nixosTesting.makeTest testFunction
+
 let
   pkgs = (import ./nix {}).pkgs;
-  testFunction = { pkgs, ... }: {
-    name = "test";
+  make-test = import "${pkgs.sources.nixpkgs}/nixos/tests/make-test.nix";
+in
+  make-test {
+    nodes = { 
+      machine = { ... }: { 
+        environment.systemPackages = [ pkgs.env-config ]; 
+        }; 
+      };
 
-    nodes = {
-    };
+    testScript =
+      ''
+        $machine->start;
+        $machine->waitForUnit("default.target");
 
-    testScript = ''
-    '';
-  };
-  nixosTesting = import "${nixpkgs}/lib/testing.nix" {
-    inherit pkgs;
-    system = "x86_64-linux";
-  };
-in nixosTesting.makeTest testFunction
+        # check that invoking the executable with the `--help` flag is supported
+        $machine->succeed("package1-exe --help");
+      '';
+  }
