33f2360bb807a77160ec2f609706e14a3428fcf9
Damian Baar <damian.baar@gmail.com>
2019-07-04 16:24:11 +0200

    rewriting deployment-scripts with ssh generation based on machine.json file, moving around some bits and pieces

--- infra/deployment-scripts.nix
+++ infra/deployment-scripts.nix
@@ -1,52 +1,73 @@
-{nixops, writeScript, writeScriptBin, rsync}:
+# Generated commands:
+# ops-deploy-ec2              
+# ops-delete-ec2              
+# ops-destroy-ec2             
+# ops-ssh-to-*
+{
+  nixops, 
+  writeScript, 
+  writeScriptBin, 
+  rsync,
+  lib,
+  machines
+}:
 let
-  deployment = {name, configuration, machine, resource-name}: rec {
-    create = writeScriptBin "create-deployment-${name}" ''
-      ${nixops}/bin/nixops list | grep "${name}" || \
-        ${nixops}/bin/nixops create ${configuration} ${machine} -d ${name}
-    '';
 
-    destroy = writeScriptBin "ops-destroy-${name}" ''
-      ${nixops}/bin/nixops destroy -d ${name}
-    '';
+  create-ssh-variants = let
+    make-ssh = name: resource-name: 
+      writeScriptBin "ops-ssh-to-${name}-${resource-name}" ''
+        ${nixops}/bin/nixops ssh -d ${name} ${resource-name}
+      '';
 
-    delete = writeScriptBin "ops-delete-${name}" ''
-      ${destroy}/bin/${destroy.name}
-      ${nixops}/bin/nixops delete -d ${name}
-    '';
+    masters = (builtins.attrNames machines.membership) ;
+    nodes = 
+      (lib.flatten 
+          (lib.foldl 
+            builtins.concatLists 
+            (builtins.attrValues machines.membership) 
+            []));
+  in
+    name: 
+      lib.genAttrs  
+        (masters ++ nodes) 
+        (make-ssh name);
 
-    deploy = writeScriptBin "ops-deploy-${name}" ''
-      ${create}/bin/${create.name}
-      ${nixops}/bin/nixops deploy -d ${name} --kill-obsolete
-    '';
+  deployment = {name, configuration, machine}: rec {
 
-    copy-contents = writeScriptBin "ops-folder-sync-${name}" ''
-      ${rsync}/bin/rsync --rsh="${make-ssh}/bin/${make-ssh.name}" $*
-    '';
+      create = writeScriptBin "create-deployment-${name}" ''
+        ${nixops}/bin/nixops list | grep "${name}" || \
+          ${nixops}/bin/nixops create ${configuration} ${machine} -d ${name}
+      '';
 
-    make-ssh = writeScriptBin "ops-ssh-to-${name}-${resource-name}" ''
-      ${nixops}/bin/nixops ssh -d ${name} ${resource-name}
-    '';
-  };
+      destroy = writeScriptBin "ops-destroy-${name}" ''
+        ${nixops}/bin/nixops destroy -d ${name}
+      '';
+
+      delete = writeScriptBin "ops-delete-${name}" ''
+        ${destroy}/bin/${destroy.name}
+        ${nixops}/bin/nixops delete -d ${name}
+      '';
+
+      deploy = writeScriptBin "ops-deploy-${name}" ''
+        ${create}/bin/${create.name}
+        ${nixops}/bin/nixops deploy -d ${name} --kill-obsolete
+      '';
+
+      # copy-contents = writeScriptBin "ops-folder-sync-${name}" ''
+      #   ${rsync}/bin/rsync --rsh="${make-ssh}/bin/${make-ssh.name}" $*
+      # '';
+    } // (create-ssh-variants name);
 in
 {
-  # INFO
-  # it is going to generate commands:
-  # ops-deploy-ec2              
-  # ops-delete-ec2              
-  # ops-destroy-ec2             
-  # ops-ssh-to-ec2-buildit-ops
   deploy-ec2 = deployment {
     name = "ec2";
     configuration = "infra/deployment.nix";
     machine = "infra/targets/ec2.nix";
-    resource-name = "buildit-ops"; # take from external config
   };
 
   deploy-vbox = deployment {
     name = "local-deployment";
     configuration = "infra/deployment.nix";
     machine = "infra/targets/vbox-cluster.nix";
-    resource-name = "buildit-ops"; # take from external config
   };
 }
