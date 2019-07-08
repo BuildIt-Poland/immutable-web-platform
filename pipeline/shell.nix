ad9aff3698fdb06cf65f2d80fdae9d4adcc80e43
Damian Baar <damian.baar@wipro.com>
2019-07-08 09:02:04 +0200

    k8s resources push to repo preparations

--- pipeline/shell.nix
+++ pipeline/shell.nix
@@ -1,13 +1,18 @@
 # worker shell
 # IMPORTANT: nix is lazy so we can require whole ./nix folder and reuse the scripts - awesome isn't it?
 {
-  pkgs ? import <nixpkgs> {}
+  pkgs ? (import ../nix { 
+    system = "x86_64-linux"; 
+  });
 }:
 let
   testScript2 = pkgs.writeScript "test-script" ''
     echo '{"foo": 0}' | ${pkgs.jq}/bin/jq .
     echo "hello test script"
+    cat ${pkgs.k8s-cluster-operations.resouces.yaml.cluster} 
+    cat ${pkgs.k8s-cluster-operations.resouces.yaml.functions} 
   '';
+
   testScript = pkgs.stdenv.mkDerivation {
     name = "test-script";
     src = ./.;
