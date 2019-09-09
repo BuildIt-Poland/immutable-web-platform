367fe71187475842a0cdc0c12d1f9f635a5a809e
Damian Baar <damian.baar@gmail.com>
2019-09-09 17:11:21 +0200

    added opa: https://github.com/open-policy-agent/opa - to be able to test polcies

---
+++ nix/tools/opa/default.nix
@@ -0,0 +1,24 @@
+{ lib, buildGoPackage, fetchFromGitHub }:
+
+buildGoPackage rec {
+  name = "opa-${version}";
+  version = "0.13.5";
+
+  src = fetchFromGitHub {
+    owner = "open-policy-agent";
+    repo = "opa";
+    rev = "v${version}";
+    sha256 = "1cwvjaxzfx2msaa1cljkm6ca7d67yi1nba0yvqk0xmnkq69nwk92";
+  };
+
+  goDeps = ./deps.nix;
+  goPackagePath = "github.com/open-policy-agent/opa";
+  modSha256 = "0a00kcyagqczw0vhl8qs2xs1y8myw080y9kjs4qrcmj6kibdy55q";
+
+  meta = with lib; {
+    description = "The Open Policy Agent (OPA) is an open source, general-purpose policy engine that enables unified, context-aware policy enforcement across the entire stack.";
+    homepage = https://github.com/open-policy-agent/opa;
+    license = licenses.asl20;
+    platforms = platforms.unix;
+  };
+}
--- policy/knative.rego
+++ policy/knative.rego
@@ -1,8 +1,8 @@
 # https://www.openpolicyagent.org/docs/latest/how-do-i-write-policies/
-package main
+package knative
 
 deny[msg] {
   input.kind = "Service"
-  not input.metadata.name2
+  not input.metadata.name
   msg = "Name has to be defined"
 }
