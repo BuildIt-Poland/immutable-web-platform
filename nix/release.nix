936e63eb98a4cd482240da5d1be011f0091167bc
Damian Baar <damian.baar@gmail.com>
2019-09-18 10:07:31 +0200

    missed one piece ...

--- nix/release.nix
+++ nix/release.nix
@@ -1,12 +1,3 @@
-{ supportedSystems ? ["x86_64-linux"], ... }:
-
-with (import <nixpkgs/pkgs/top-level/release-lib.nix> { inherit supportedSystems; });
-{
-  hello_world_3 = pkgs.lib.genAttrs supportedSystems (system: (pkgsFor system).hello);
-
-} // mapTestOn {
-
-  # Fancy shortcut to generate one attribute per supported platform.
-  hello = supportedSystems;
-
+{ nixpkgs }: let pkgs = import nixpkgs {}; in {
+  inherit (pkgs) hello;
 }
