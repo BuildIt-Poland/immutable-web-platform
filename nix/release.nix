099946770a75cdea34b29f30f75438917f04054e
Damian Baar <damian.baar@gmail.com>
2019-09-18 09:23:07 +0200

    docs lying ...

--- nix/release.nix
+++ nix/release.nix
@@ -1,3 +1,12 @@
-{ nixpkgs }: let pkgs = import nixpkgs {}; in {
-  inherit (pkgs) hello;
+{ supportedSystems ? ["x86_64-linux"], ... }:
+
+with (import <nixpkgs/pkgs/top-level/release-lib.nix> { inherit supportedSystems; });
+{
+  hello_world_3 = pkgs.lib.genAttrs supportedSystems (system: (pkgsFor system).hello);
+
+} // mapTestOn {
+
+  # Fancy shortcut to generate one attribute per supported platform.
+  hello = supportedSystems;
+
 }
