9c035a90e33088cd5c4f5d8c660d3325935a3e9b
Damian Baar <damian.baar@gmail.com>
2019-09-19 16:49:41 +0200

    yarn2nix - using fork instead of patch - super close to switch to node2nix ...

--- packages/brigade/brigade-extension/nix/package.nix
+++ packages/brigade/brigade-extension/nix/package.nix
@@ -1,9 +1,9 @@
 { pkgs, gitignore }:
 pkgs.yarn2nix.mkYarnPackage {
   name = "brigade-extension";
-  src = ./.;
-  packageJson = ./package.json;
-  yarnLock = ./yarn.lock;
+  src = ./..;
+  packageJson = ../package.json;
+  yarnLock = ../yarn.lock;
   postBuild = ''
     yarn run build
   '';
