6d8c3c240466b237478a4696102a1bf12ab5ef6d
Damian Baar <damian.baar@wipro.com>
2019-09-19 11:32:52 +0200

    removing gitignore from briagede-extension - patch for yarn2nix does not helped

--- packages/brigade/brigade-extension/nix/package.nix
+++ packages/brigade/brigade-extension/nix/package.nix
@@ -1,7 +1,7 @@
 { pkgs, gitignore }:
 pkgs.yarn2nix.mkYarnPackage {
   name = "brigade-extension";
-  src = gitignore ./..;
+  src = ./..;
   packageJson = ../package.json;
   yarnLock = ../yarn.lock;
   postBuild = ''
