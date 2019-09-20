06d958e509b580c0db8b850662fc8eb3333f14b5
Damian Baar <damian.baar@wipro.com>
2019-09-20 09:01:16 +0200

    folder path

--- packages/brigade/brigade-extension/nix/package.nix
+++ packages/brigade/brigade-extension/nix/package.nix
@@ -1,9 +1,9 @@
 { pkgs, gitignore }:
 pkgs.yarn2nix.mkYarnPackage {
   name = "brigade-extension";
-  src = ./.;
-  packageJSON = ./package.json;
-  yarnLock = ./yarn.lock;
+  src = ./..;
+  packageJSON = ../package.json;
+  yarnLock = ../yarn.lock;
   postBuild = ''
     yarn run build
   '';
