3368a05bc2aaa84a41efecbb93cf8c8b66946f7d
Damian Baar <damian.baar@wipro.com>
2019-09-20 23:40:16 +0200

    docker works and builds remotly - this is how in kubernetes it is going to work

--- build-test-docker-file.sh
+++ build-test-docker-file.sh
@@ -1,5 +1,7 @@
 docker build -f functions/express-app/Dockerfile . \
   --network=host  \
+  --build-arg ssh_prv_key="$(cat ~/.ssh/id_rsa)" \
+  --build-arg ssh_pub_key="$(cat ~/.ssh/id_rsa.pub)" \
   --build-arg AWS_ACCESS_KEY_ID="$(aws configure get aws_access_key_id)" \
   --build-arg AWS_SECRET_ACCESS_KEY="$(aws configure get aws_secret_access_key)" \
   --build-arg AWS_DEFAULT_REGION="$(aws configure get region)"
