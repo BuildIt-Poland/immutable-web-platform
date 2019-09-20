7500ebd2d753f24db5d5d49fc6ff7a62c9c9cdf0
Damian Baar <damian.baar@gmail.com>
2019-09-20 09:37:47 +0200

    changing to spot instance and using xlarge

--- terraform/modules/aws-nix-ec2/ec2.tf
+++ terraform/modules/aws-nix-ec2/ec2.tf
@@ -9,7 +9,10 @@ resource "aws_key_pair" "instance" {
   public_key = file("${var.ssh_pub_key}")
 }
 
-resource "aws_instance" "nixos_instance" {
+# FIXME should be flag
+# In case of nix stuff it make sense as we are keeping binary store in s3
+# so we can recreate anything fast
+resource "aws_spot_instance_request" "nixos_instance" {
   ami           = module.aws_image_nixos.ami
   instance_type = var.instance_type
   key_name      = aws_key_pair.instance.key_name
@@ -18,6 +21,10 @@ resource "aws_instance" "nixos_instance" {
   subnet_id              = var.subnet_id
   associate_public_ip_address = true
   iam_instance_profile = var.iam_instance_profile
+
+  spot_price    = var.spot_price
+  wait_for_fulfillment = true
+  spot_type = "one-time"
 
   root_block_device {
     delete_on_termination = true
