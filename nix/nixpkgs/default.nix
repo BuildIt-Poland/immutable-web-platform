71ea00059fc754158911cc8ee2df27272359be14
Damian Baar <damian.baar@wipro.com>
2019-09-26 10:55:06 +0200

    still apply patches ...

--- nix/nixpkgs/default.nix
+++ nix/nixpkgs/default.nix
@@ -1,15 +1,29 @@
-{sources, pkgs ? import <nixpkgs> {}, args ? {}}:
+{sources, pkgs ? import <nixpkgs> {}, extraArgs ? {}}:
 with pkgs;
 let
   hostPkgs = import <nixpkgs> {};
-  pinned = import (hostPkgs.applyPatches {
-    src = fetchFromGitHub {
-      sha256 = sources.nixpkgs.sha256;
-      repo = sources.nixpkgs.repo;
-      owner = sources.nixpkgs.owner;
-      rev = sources.nixpkgs.rev;
-    };
-    patches = [./go-modules.patch];
-  }) ({} // args);
+  # pinned = import (hostPkgs.applyPatches {
+  #   src = fetchFromGitHub {
+  #     sha256 = sources.nixpkgs.sha256;
+  #     repo = sources.nixpkgs.repo;
+  #     owner = sources.nixpkgs.owner;
+  #     rev = sources.nixpkgs.rev;
+  #   };
+  # }) ({} // extraArgs);
+  patches = [./go-modules.patch];
+  pinnedPkgs = sources.nixpkgs;
+  patchedPkgs = hostPkgs.runCommand "nixpkgs-${pinnedPkgs.rev}"
+    {
+      inherit pinnedPkgs;
+      inherit patches;
+    }
+    ''
+      cp -r $pinnedPkgs $out
+      chmod -R +w $out
+      for p in $patches; do
+        echo "Applying patch $p";
+        patch -d $out -p1 < "$p";
+      done
+    '';
 in
-  pinned
+  import patchedPkgs extraArgs
