2e811100eeff6cf8f07ed176cd248d698d5e10ef
Damian Baar <damian.baar@gmail.com>
2019-10-01 15:45:04 +0200

    aliases - it has to work about differently - zsh aliases

--- nix/modules/integration/base.nix
+++ nix/modules/integration/base.nix
@@ -134,14 +134,13 @@ in
             ${log.info "Your domain is: ${config.project.domain}"}
             ${log.info "Your runtime perspective is: ${config.environment.perspective}"}
           '';
-          # TODO make configurable from module perspective
-          # error: /usr/bin/alias: line 4: builtin: rc: not a shell builtin
-          aliases = ''
-            alias k='kubectl'
-            alias kd='kubectl describe'
-            alias kg='kubectl get'
-            alias kl='kubectl logs'
-          '';
+          # TODO make configurable from module perspective or folder
+          # aliases = ''
+          #   alias k='kubectl'
+          #   alias kd='kubectl describe'
+          #   alias kg='kubectl get'
+          #   alias kl='kubectl logs'
+          # '';
 
           footer = ''
             echo "Run 'get-help' to get all available commands"
@@ -158,7 +157,6 @@ in
           ${config.actions.list}
 
           ${footer}
-          ${aliases}
         '';
     })
 
